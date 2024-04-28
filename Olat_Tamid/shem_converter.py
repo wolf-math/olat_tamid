c8ef9f0c2efced7a2cc6c2d8a4a611db333facc0
Aaron Wolf <aaron.wolf@gmail.com>
2024-04-28 11:28:11 +0300

    tamid

---
+++ Olat_Tamid/shem_converter.py
@@ -0,0 +1,69 @@
+aleph_bet = ['א','ב','ג','ד','ה','ו','ז','ח','ט','י','כ','ך','ל','מ','ם',
+             'נ','ן','ס','ע','פ','ף','צ','ק','ר','ש','ת','װ','ױ','ײ','יִ',
+             'ﬡ','ﬢ','ﬣ','ﬤ','ﬥ','ﬦ','ﬧ','ﬨ','שׁ','שׂ','שּׁ','שּׂ','אַ','אָ',
+             'גּ','דּ','הּ','וּ','זּ','טּ','יּ','ךּ','כּ','לּ','מּ','נּ','סּ','ףּ',
+             'פּ','צּ','שּ','תּ','וֹ','בֿ','כֿ','פֿ','ﭏ']
+
+cantellation_marks = ['֑','֒','֓','֔','֕','֖','֗','֘','֙','֚','֛','֜',
+             '֝','֞','֠','֡','֢','֣','֤','֥','֦','֧','֨','֩','֪','֫','֬','֭','֯','׃']
+
+vowels = ['ְ','ֱ','ֲ','ֳ','ִ','ֵ','ֶ','ַ','ָ','ֹ','ֺ','ֻ','ּ','ֽ','־','ֿ','ׁ','ׂ','ׄ','ׅ']
+
+shem = 'יהוה'
+
+## Strips vowels and cantelation marks from words
+def nonalpha_remover(word):
+    no_cant_word = ''
+    for letter in word:
+        if letter.isalpha() == True:
+            no_cant_word = no_cant_word + letter
+    return no_cant_word
+
+## Converts Shem-Havaya to double-yud while preserving cantelation marks
+def shem_converter(word):
+    prefix = ''
+    if word[0] != aleph_bet[9]:
+        prefix = prefix + word[0]
+        if word[1].isalpha() == False:
+            prefix = prefix + word[1]
+            if word[2].isalpha() == False:
+                prefix = prefix + word[2]
+    no_prefix_word = word[len(prefix):]
+
+    if len(prefix) > 0:
+        yud1 = aleph_bet[9]
+    else:
+        yud1 = aleph_bet[9]+vowels[0]
+
+    yud2 = aleph_bet[9]+vowels[8]
+
+    cant_marks = []
+
+    for character in no_prefix_word:
+        if character in cantellation_marks:
+            cant_marks.append(character)
+    if len(cant_marks) == 0:
+        new_shem = prefix + yud1 + yud2
+    elif len(cant_marks) == 1:
+        new_shem = prefix + yud1 + yud2 + cant_marks[0]
+    elif len(cant_marks) == 2:
+        new_shem = prefix + yud1 + cant_marks[0] + yud2 + cant_marks[1]
+
+    return new_shem
+
+## Creates new paragraph with double-yud in place of Shem Havaya
+
+def con(paragraph):
+    paragraph = str.replace(paragraph, '־', ' ־ ')
+    par_list = paragraph.split()
+    for word in par_list:
+        index = par_list.index(word)
+        if shem in nonalpha_remover(word):
+            double_yud = shem_converter(word)
+            par_list[index] = double_yud
+
+    new_par = ' '.join(par_list)
+    new_par = str.replace(new_par, ' ־ ','־')
+    new_par = str.replace(new_par, '׃', '׃ \n')
+    return(new_par)
+
--- Olat_Tamid/tanakh_reader.py
+++ Olat_Tamid/tanakh_reader.py
@@ -1,24 +1,44 @@
 import json, sys
 
-def tanakh_reader(section, book, chapter=None, verse=None):
-    chapter = int(chapter) - 1
-    verse = int(verse) - 1
+def verse(book, chapter, verse):
+    chapter_index = int(chapter) - 1
+    verse_index = int(verse) - 1
+    
     with open(f"Tanakh_niqqud.json", "r") as text:
         data = json.load(text)
-        # print(data['torah']['genesis']['text'][0][0])
-        if verse:
-            return (f"{book} {chapter}:{verse}", data[section][book]['text'][chapter][verse])
-        elif chapter:
-            return (f"{book} {chapter}", data[section][book]['text'][chapter])
-        else:
-            return (data[section][book]['text'])
 
-# if __name__ == '__main__':
-#     verse = sys.argv[4] if len(sys.argv) > 4 else None
-#     chapter = sys.argv[3] if len(sys.argv) > 3 else None
-#     book = sys.argv[2] if len(sys.argv) > 2 else None
-#     section = sys.argv[1] if len(sys.argv) > 1 else None
-#     # print(verse, chapter, book, section)
-#     tanakh_reader(section, book, chapter, verse)
+    if book not in data or chapter_index >= len(data[book]['text']) or verse_index >= len(data[book]['text'][chapter_index]):
+        return "Oops something went wrong"
+        
+    return (f"{book} {chapter}:{verse}", data[book]['text'][chapter_index][verse_index])
+
+
+def chapter(book, chapter):
+    chapter_index = int(chapter) - 1
     
+    with open(f"Tanakh_niqqud.json", "r") as text:
+        data = json.load(text)
+
+    if book not in data or chapter_index >= len(data[book]['text']):
+        return "Oops something went wrong"
+
+    chapter_list = data[book]['text'][chapter_index]
+    chapter_string = ' '.join(chapter_list)
+
+    return (f"{book} {chapter}", chapter_string)
+
+
+def verses(book, chapter, verse, count):
+    chapter_index = int(chapter) - 1
+    verse_index = int(verse) - 1
+
+    with open("Tanakh_niqqud.json", "r") as text:
+        data = json.load(text)
+
+    if book not in data or chapter_index >= len(data[book]['text']) or verse_index >= len(data[book]['text'][chapter_index]):
+        return "Oops something went wrong"
+
+    verses_list = data[book]['text'][chapter_index][verse_index: verse_index + count]
+    verses_string = ' '.join(verses_list)
     
+    return (f"{book} {chapter}:{verse} - {verse + count - 1}", verses_string)
