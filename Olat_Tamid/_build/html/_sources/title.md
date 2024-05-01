208bd3591f9b0bbfcba710902eafffc384d04af0
Aaron Wolf <aaron.wolf@gmail.com>
2024-05-01 23:02:05 +0300

    shacharit started

--- Olat_Tamid/_build/html/_sources/title.md
+++
@@ -1,9 +0,0 @@
-# סידור עלת תמיד
-## לבני אשכנז
-<!-- \vspace{.5in}
-\includegraphics[scale=.5]{wolf_shofar_bitmap.png}
- }
- -->
- מסודר על ידי 
-אהרן דוד וואלף
-מנהג אשכנז
--- Olat_Tamid/tanakh_reader.py
+++ Olat_Tamid/tanakh_reader.py
@@ -1,10 +1,13 @@
-import json, sys
+import json, sys, os
+
+module_dir = os.path.dirname(__file__)
+tanakh = os.path.join(module_dir, 'Tanakh_niqqud.json')
 
 def verse(book, chapter, verse):
     chapter_index = int(chapter) - 1
     verse_index = int(verse) - 1
     
-    with open(f"Tanakh_niqqud.json", "r") as text:
+    with open(tanakh, "r") as text:
         data = json.load(text)
 
     if book not in data or chapter_index >= len(data[book]['text']) or verse_index >= len(data[book]['text'][chapter_index]):
@@ -16,7 +19,7 @@ def verse(book, chapter, verse):
 def chapter(book, chapter):
     chapter_index = int(chapter) - 1
     
-    with open(f"Tanakh_niqqud.json", "r") as text:
+    with open(tanakh, "r") as text:
         data = json.load(text)
 
     if book not in data or chapter_index >= len(data[book]['text']):
@@ -33,7 +36,7 @@ def verses(book, chapter, start, stop):
     start_index = int(start) - 1
     stop_index = int(stop) - 1
 
-    with open("Tanakh_niqqud.json", "r") as text:
+    with open(tanakh, "r") as text:
         data = json.load(text)
 
     if book not in data or chapter_index >= len(data[book]['text']) or start_index >= len(data[book]['text'][chapter_index]):
